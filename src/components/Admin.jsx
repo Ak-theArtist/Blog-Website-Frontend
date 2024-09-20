9b36bff7d71321493ad0a54dcf843c35bedb0238
Akash Kumar <kumarakash91384@gmail.com>
2024-09-20 10:46:25 +0530

    final

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -5,16 +5,17 @@ const Admin = () => {
     const [users, setUsers] = useState([]);
     const [email, setEmail] = useState([]);
 
-    useEffect(() => {
-        axios.get('https://blog-website-backend-9nth.onrender.com/getAllusers')
-            .then(response => {
-                const filteredUsers = response.data.filter(user => user.role === 'user');
-                const filteredEmail = response.data.filter(user => user.role === 'user');
-                setUsers(filteredUsers);
-                setEmail(filteredEmail);
-            })
-            .catch(err => console.error(err));
-    }, []);  
+    axios.get('https://blog-website-backend-9nth.onrender.com/getAllusers')
+        .then(response => {
+            console.log(response); 
+            const filteredUsers = response.data.filter(user => user.role === 'user');
+            setUsers(filteredUsers);
+        })
+        .catch(err => {
+            console.error(err);
+            alert("Failed to fetch users. Please check the console for more details.");
+        });
+
 
     const handleDelete = (userId) => {
         if (window.confirm('Are you sure you want to delete this user?')) {
